05ba9b71342673f11d4f1582e9eb10d257d11d3f
754594774 <baishikele2016>
2018-06-01 22:01:25 +0800

    1，ckeditor添加代码高亮 2，文章详情页面添加代码高亮（angularjs不支持）

--- src/main/webapp/plugins/ckeditor/config.js
+++ src/main/webapp/plugins/ckeditor/config.js
@@ -16,13 +16,16 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
 		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
 		{ name: 'links', items: [ 'Link', 'Unlink' ] },
-		{ name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar' ] }
+		{ name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'CodeSnippet'  ] }
 	];
 
 		config.extraAllowedContent='h3{clear};h2{line-height};h2 h3{margin-left,margin-top}',
 
 		// Adding drag and drop image upload.
-		config.extraPlugins= 'print,format,font,colorbutton,justify,uploadimage',
+		config.extraPlugins= 'print,format,font,colorbutton,justify,uploadimage,codesnippet',
+		//使用zenburn的代码高亮风格样式 PS:zenburn效果就是黑色背景
+		//如果不设置着默认风格为default
+		config.codeSnippet_theme= 'monokai_sublime',
 		config.uploadUrl= '/upload?command=QuickUpload&type=Files&responseType=json',
 
 		// Configure your file manager integration. This example uses CKFinder 3 for PHP.
